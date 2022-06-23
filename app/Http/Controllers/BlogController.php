ddd49ff8d2eb51faba3cf5c3363329340749a937
Saker-Hossain <sakerhossain2017@gmail.com>
2022-06-23 11:55:18 +0600

    Frontend Display of Categories Dynamically

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,8 +22,8 @@ class BlogController extends Controller
         }else {
             $posts = Post::latest()->paginate(4);
         }
-
-        return view('blogPost.blog', compact('posts'));
+        $categories = Category::all();
+        return view('blogPost.blog', compact('posts', 'categories'));
     }
 
     public function create()
